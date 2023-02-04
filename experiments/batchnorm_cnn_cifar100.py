bfcf4ef8ad24c2e6b78fd20f6f1d28baa1683a75
liam altarac <liamaltarac@gmail.com>
2023-02-04 13:24:40 -0500

    added gitignore

---
+++ experiments/batchnorm_cnn_cifar100.py
@@ -0,0 +1,76 @@
+import tensorflow as tf
+from tensorflow import keras
+import sys
+
+if __name__ == '__main__':
+
+
+    sys.path.append("..")    
+
+    from .models.batch_norm import batchnorm_cnn
+    from geo_init.geometric_initialization_relu import GeometricInit3x3Relu
+
+    from tensorflow.python.client import device_lib
+    print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
+    print(tf.__version__ )
+
+    model = batchnorm_cnn(k_init = GeometricInit3x3Relu)
+
+    num_classes = 100
+    input_shape = (32, 32, 3)
+
+    # Load the data and split it between train and test sets
+    (x_train, y_train), (x_test, y_test) = keras.datasets.cifar100.load_data()
+
+    x_train = x_train.astype("float32") / 255
+    x_test = x_test.astype("float32") / 255
+
+    from sklearn.model_selection import train_test_split
+    from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+    # Configuration for creating new images
+    train_datagen = ImageDataGenerator(
+        rotation_range=20,
+        horizontal_flip=True,
+    )
+
+    X_train, X_validation, y_train, y_validation = train_test_split(x_train, y_train, test_size=0.2, random_state=93)
+    train_datagen.fit(X_train)
+
+    optimizer = tf.keras.optimizers.RMSprop(learning_rate=1e-4)
+
+    import wandb
+    from wandb.keras import WandbCallback
+
+    wandb.init(project="Test Weights and Gradient Histogram", entity="geometric_init")
+    wandb.run.name = '8_layer_batchNorm_cifar100_OURS'
+    wandb.config = {
+    "learning_rate": 1e-4,
+    'batch_size' : 64,
+    'epochs' : 10,
+    "initialization": "GeometricInit3x3Relu",
+    "model": '8_layer_BatchNorm CNN'
+    }
+
+    model.compile(
+            optimizer=optimizer,
+            loss=keras.losses.SparseCategoricalCrossentropy(from_logits=False),
+            metrics=[
+                keras.metrics.SparseCategoricalAccuracy(name="accuracy"),
+                keras.metrics.SparseTopKCategoricalAccuracy(5, name="top-5-accuracy"),
+            ],
+    )
+    batch_size = 64
+    epochs = 10
+    history = model.fit(X_train, 
+                        y_train, 
+                        batch_size=batch_size, 
+                        epochs=epochs, 
+                        validation_data=(X_validation, y_validation), 
+                        callbacks=[WandbCallback(log_gradients   = (True), 
+                                                 log_weights     = (True),
+                                                 training_data   = (X_train, y_train),
+                                                 validation_data = (X_validation, y_validation),
+                                                 input_type = "images",
+                                                 output_type = "label",
+                                                 log_batch_frequency = 10)])
---
+++ experiments/models/batch_norm.py
@@ -0,0 +1,62 @@
+    #ch norm model 4
+'''
+From :https://github.com/LeoTungAnh/CNN-CIFAR-100/blob/main/CNN_models.ipynb
+
+'''
+from keras.models import Sequential
+from keras.layers.convolutional import Conv2D
+from keras.layers.pooling import MaxPool2D
+from keras.layers.core import Dense,Activation,Dropout,Flatten
+
+from tensorflow.keras.layers import BatchNormalization
+from tensorflow.keras.initializers import RandomNormal, Constant
+
+def batchnorm_cnn(k_init = 'he_normal'):
+
+    model = Sequential()
+    
+    model.add(Conv2D(256,(3,3),padding='same',input_shape=(32,32,3), kernel_initializer = k_init))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(Conv2D(256,(3,3),padding='same', kernel_initializer = k_init))
+    model.add(Activation('relu'))
+    model.add(MaxPool2D(pool_size=(2,2)))
+    model.add(Dropout(0.2))
+    
+    model.add(Conv2D(512,(3,3),padding='same', kernel_initializer = k_init))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(Conv2D(512,(3,3),padding='same', kernel_initializer = k_init))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(MaxPool2D(pool_size=(2,2)))
+    model.add(Dropout(0.2))
+
+    model.add(Conv2D(512,(3,3),padding='same', kernel_initializer = k_init))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(Conv2D(512,(3,3),padding='same', kernel_initializer = k_init))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(MaxPool2D(pool_size=(2,2)))
+    model.add(Dropout(0.2))
+
+    model.add(Conv2D(512,(3,3),padding='same', kernel_initializer = k_init))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(Conv2D(512,(3,3),padding='same', kernel_initializer = k_init))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(MaxPool2D(pool_size=(2,2)))
+    model.add(Dropout(0.2))
+
+    model.add(Flatten())
+    model.add(Dense(1024))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.2))
+    model.add(BatchNormalization(momentum=0.95, 
+            epsilon=0.005,
+            beta_initializer=RandomNormal(mean=0.0, stddev=0.05), 
+            gamma_initializer=Constant(value=0.9)))
+    model.add(Dense(100,activation='softmax'))
+    return model        
